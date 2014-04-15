dc3264264e3d9973df6f606430274e0b1fc27450
'Michael <mchuang327@gmail.com>
2014-04-15 13:09:18 -0700

    kvcache tests

--- kvstore/test/kvstore/KVCacheTest.java
+++ kvstore/test/kvstore/KVCacheTest.java
@@ -16,4 +16,49 @@ public class KVCacheTest {
         assertEquals("world", cache.get("hello"));
     }
 
+    /**
+     * Verify the cache can put and replace a KV pair successfully.
+     */
+    @Test
+    public void singlePutAndReplace() {
+    	KVCache cache = new KVCache(1, 4);
+        cache.put("hello", "world");
+        assertEquals("world", cache.get("hello"));
+        cache.put("hello", "friend");
+        assertEquals("friend", cache.get("hello"));
+    }
+    
+    /**
+     * Verify the cache can put and delete a KV pair successfully.
+     */
+    @Test
+    public void singlePutAndDel() {
+    	KVCache cache = new KVCache(1, 4);
+        cache.put("hello", "world");
+        assertEquals("world", cache.get("hello"));
+        cache.del("hello");
+        assertEquals(null, cache.get("hello"));
+    }
+    
+    /**
+     * Verify the cache implements second chance correctly.
+     */
+    @Test
+    public void secondChance() {
+    	KVCache cache = new KVCache(1, 4);
+    	cache.put("1", "one");
+    	cache.put("2", "two");
+    	cache.put("3", "three");
+    	cache.put("4", "four");
+    	cache.get("1");
+    	cache.get("2");
+    	cache.get("4");
+    	cache.put("5", "five");
+    	assertEquals(null, cache.get("3"));
+    	assertEquals("five", cache.get("5"));
+    	assertEquals("one", cache.get("1"));
+    	assertEquals("two", cache.get("2"));
+    	assertEquals("four", cache.get("4"));
+    }
+    
 }
