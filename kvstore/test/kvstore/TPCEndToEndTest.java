2b4cb833fffb1c1c0c2fb91472ab248595982545
'Michael <mchuang327@gmail.com>
2014-05-08 23:25:27 -0700

    tpc logging and rebuilding test

--- kvstore/test/kvstore/TPCEndToEndTest.java
+++ kvstore/test/kvstore/TPCEndToEndTest.java
@@ -1,6 +1,12 @@
 package kvstore;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -22,4 +28,27 @@ public class TPCEndToEndTest extends TPCEndToEndTemplate {
     		assertEquals("del failed",  KVConstants.ERROR_NO_SUCH_KEY, kve.getKVMessage().getMessage());
     	}
     }
+    
+    @Test(timeout = 15000)
+    public void test0() throws KVException {
+    	try {
+	        client.put("foo", "bar");
+	        client.get(null);
+    	} catch (KVException kve) {
+    		assertEquals("del failed",  KVConstants.ERROR_INVALID_KEY, kve.getKVMessage().getMessage());
+    	}
+    }
+    
+    @Test(timeout = 15000)
+    public void test1() throws KVException {
+    	try {
+	        client.put("foo", "bar");
+	        assertEquals(client.get("foo"), "bar");
+	        client.put("hello", "world");
+	        assertEquals(client.get("hello"), "world");
+    	} catch (KVException kve) {
+    		
+    	}
+    }
+    
 }
--- kvstore/test/kvstore/TPCLogTest.java
+++ kvstore/test/kvstore/TPCLogTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
-public class TPCLogTest {
+public class TPCLogTest extends TPCEndToEndTemplate{
 	/**
      * Sanity test that TPCLog can be rebuilt correctly.
      */
@@ -29,12 +29,47 @@ public class TPCLogTest {
     		
     		log.rebuildServer();
     		server = log.getServer();
-    		System.out.println(server.toString());
+    		//System.out.println(server.toString());
     		assertEquals("world", server.get("hello"));
     		assertEquals("bar", server.get("foo"));
     		
     	} catch (KVException kve) {
     		fail("Test should not fail");
+    	}
+    }
+    
+    @Test
+    public void setup() {
+    	try {
+	    	client.put("one", "1");
+	    	client.put("two", "2");
+	    	client.put("three", "3");
+	    	client.put("four", "4");
+	    	client.put("five", "5");
+	    	client.put("six", "6");
+	    	//client.put("seven", "7");
+	    	client.del("six");
+	    	client.put("eight", "8");
+    	} catch (KVException kve) {
+    		
+    	}
+    }
+    
+    //Don't forget to delete the log files before running this test in bin folder
+    @Test
+    public void check() {
+    	try {
+	    	assertEquals(client.get("one"), "1");
+	    	assertEquals(client.get("two"), "2");
+	    	assertEquals(client.get("three"), "3");
+	    	assertEquals(client.get("four"), "4");
+	    	assertEquals(client.get("five"), "5");
+	    	//assertEquals(client.get("six"), "6");
+	    	//assertEquals(client.get("seven"), "7");
+	    	assertEquals(client.get("eight"), "8");
+	    	client.get("six");
+    	} catch (KVException kve) {
+    		assertEquals(kve.getKVMessage().getMessage(), KVConstants.ERROR_NO_SUCH_KEY);
     	}
     }
 }
