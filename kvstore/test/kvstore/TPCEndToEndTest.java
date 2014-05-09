8412d056664ea562112926ad50a40cddae2a80e6
'Michael <mchuang327@gmail.com>
2014-05-08 21:19:39 -0700

    putdel test

--- kvstore/test/kvstore/TPCEndToEndTest.java
+++ kvstore/test/kvstore/TPCEndToEndTest.java
@@ -11,5 +11,11 @@ public class TPCEndToEndTest extends TPCEndToEndTemplate {
         client.put("foo", "bar");
         assertEquals("get failed", client.get("foo"), "bar");
     }
-
+    
+    @Test(timeout = 15000)
+    public void testPutDel() throws KVException {
+        client.put("foo", "bar");
+        client.del("foo");
+        assertEquals("del failed", client.get("foo"), KVConstants.ERROR_NO_SUCH_KEY);
+    }
 }
